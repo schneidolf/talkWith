8c0a73d572c46aabbf13e658268b449900fe32d0
Marco Wolf <marcowolf1987@googlemail.com>
2017-10-09 15:23:20 +0200

    Update talkwith-thingspeak.php

--- tests/talkwith-thingspeak.php
+++ tests/talkwith-thingspeak.php
@@ -5,6 +5,10 @@ require '../vendor/autoload.php';
 
 use TalkWith\TalkWithServices\TalkWithThingspeak;
 
+$channel = 'your channel number';
+$write_api_key = 'your write_api_key';
+$read_api_key = 'your read_api_key';
+
 $thingspeak = new TalkWithThingspeak($channel, $write_api_key, $read_api_key);
 
 $thingspeak->saySomething([
